729d9e35b88501a2bee27e1a1583a05574ca320d
Documenter.jl <documenter@juliadocs.github.io>
2022-06-29 23:54:28 +0000

    build based on b8ed45c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BeonDemo.jl","page":"BeonDemo.jl","title":"BeonDemo.jl","text":"","category":"section"},{"location":"","page":"BeonDemo.jl","title":"BeonDemo.jl","text":"Documentation for BeonDemo.jl","category":"page"}]
+}
